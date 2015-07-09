13aa0ce12577a6473b2aa2360bf93ac6522161ed
Ryan Wang <Ryan@iMac.local>
2015-07-09 00:27:09 -0400

    Files

---
+++ Java Exception Handling/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+class Solution{
+
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNextInt())
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            myCalculator M = new myCalculator();
+            try
+            {
+                System.out.println(M.power(n,p));
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+
+    }
+}
+
+
---
+++ Java Exception Handling/myCalculator.java
@@ -0,0 +1,15 @@
+class myCalculator {
+    
+    myCalculator() {
+        
+    }
+    
+    public int power(int n, int p) throws Exception {
+        if ((n < 0) || (p < 0)) {
+            throw new Exception("n and p should be non-negative");
+        }
+        else {
+            return (int)(Math.pow(n, p));
+        }
+    }
+}
