f1bdefd87a29b54cbc8959b6c12deddafe3bea3b
theperfectgamer <52358675+theperfectgamer@users.noreply.github.com>
2020-05-06 16:23:50 +0530

    Update Solution.java

--- Java End-Of-File/Solution.java
+++ Java End-Of-File/Solution.java
@@ -1,22 +1,14 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    Scanner userInput = new Scanner(System.in);
-    int lineNum = 1;    
-    
-    while (userInput.hasNextLine() == true) {
-    if (userInput.hasNext() == true) {
-        System.out.println(lineNum + " " + userInput.nextLine());
-        lineNum++;
-    }
-    }
-    
+        Scanner input=new Scanner(System.in);
+        int line=1;
+        while(input.hasNextLine()){
+                System.out.println(line+" "+input.nextLine());
+                line++;
+        }
     }
 }
